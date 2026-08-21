f556771870553281202994665c5e3b2b85302c15
Aryan Shrivastava <asguy29@gmail.com>
2026-08-21 12:00:05 +0530

    Add: design-hashset - submission-1

---
+++ Data Structures & Algorithms/design-hashset/submission-1.cpp
@@ -0,0 +1,21 @@
+class MyHashSet {
+public:
+
+    bool hash[1000001] = {};
+
+   MyHashSet(){
+
+   }
+
+   void add(int key){
+    hash[key]=true;
+   }
+
+   void remove(int key){
+    hash[key]=false;
+   }
+
+   bool contains(int key){
+    return hash[key];
+   }
+};
