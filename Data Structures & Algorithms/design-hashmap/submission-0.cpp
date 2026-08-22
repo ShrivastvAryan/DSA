effdfa543b1b3ada00d5d2c06b01585e62985f55
Aryan Shrivastava <asguy29@gmail.com>
2026-08-22 10:15:10 +0530

    Add: design-hashmap - submission-0

---
+++ Data Structures & Algorithms/design-hashmap/submission-0.cpp
@@ -0,0 +1,56 @@
+class MyHashMap {
+private:
+    static const int SIZE = 1000;
+
+    vector<vector<pair<int, int>>> table;
+
+    int hash(int key) {
+        return key % SIZE;
+    }
+
+public:
+    MyHashMap() {
+        table.resize(SIZE);
+    }
+
+    void put(int key, int value) {
+        int index = hash(key);
+
+        // Check if key already exists
+        for (auto &p : table[index]) {
+            if (p.first == key) {
+                p.second = value;  // update value
+                return;
+            }
+        }
+
+        // Key doesn't exist, insert it
+        table[index].push_back({key, value});
+    }
+
+    int get(int key) {
+        int index = hash(key);
+
+        for (auto &p : table[index]) {
+            if (p.first == key) {
+                return p.second;
+            }
+        }
+
+        return -1;
+    }
+
+    void remove(int key) {
+        int index = hash(key);
+
+        for (auto it = table[index].begin();
+             it != table[index].end();
+             ++it) {
+
+            if (it->first == key) {
+                table[index].erase(it);
+                return;
+            }
+        }
+    }
+};
